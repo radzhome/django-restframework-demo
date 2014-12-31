53e731bb50e06af0cbc9ed1210fa1deba26ec555
Nefernefernefer <radzhome@gmail.com>
2014-12-31 18:16:52 -0500

    fix import errors

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,6 +1,8 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-#from django.conf.urls import include
+from django.conf.urls import include
 
 from snippets import views
 
