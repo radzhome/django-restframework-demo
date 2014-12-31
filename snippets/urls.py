c5f9116d9c47e0c43d87743531f3a15333ed0aa7
Nefernefernefer <radzhome@gmail.com>
2014-12-31 18:00:21 -0500

    added highlight and root views

--- snippets/urls.py
+++ snippets/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
